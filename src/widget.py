fbe063ed15564ace5f79a1c70bfd3a4b5eb44dd1
Viktor <Nekrontirr@yandex.ru>
2025-09-16 22:31:26 +0300

    Add widget.py module

---
+++ src/widget.py
@@ -0,0 +1,7 @@
+from masks import get_mask_card_number, get_mask_account
+
+def mask_account_card(account_card: str) -> str:
+    """
+     Принимает строку содержащую тип и номер карты или счета, возвращает строку с замаскированным номером или счётом
+    """
+    pass
