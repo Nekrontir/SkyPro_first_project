5be79f759b28c2d9be81deeb6e8ca37e0ec5dd5b
Viktor <Nekrontirr@yandex.ru>
2025-11-03 22:29:03 +0300

    add log_to_file_or_console function

--- src/decorators.py
+++ src/decorators.py
@@ -1,3 +1,29 @@
-@log (filename):
-def log (filename):
-    pass
+
+from typing import Optional, Callable, Any
+from functools import wraps
+
+
+
+def log_to_file_or_console(filename: Optional[str] = None) -> Callable:
+    def log_decorator (func: Callable) -> Callable:
+        @wraps(func)
+        def wrapper (*args, **kwargs) -> Any:
+            if filename is None:
+                try:
+                    result = func(*args, **kwargs)
+                    print(f"{func.__name__} ok!")
+                    return result
+                except Exception as error:
+                    print(f"{func.__name__} error: {error}. Inputs: {args}, {kwargs}. ")
+                    raise
+            else:
+                with open(filename, "a") as file:
+                    try:
+                        result = func(*args, **kwargs)
+                        file.write(f"{func.__name__} ok!\n")
+                        return result
+                    except Exception as error:
+                        file.write(f"{func.__name__} error: {error}. Inputs: {args}, {kwargs}. \n")
+                        raise
+        return wrapper
+    return log_decorator
